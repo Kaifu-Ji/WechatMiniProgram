b1ebb4cd6959de43bbe654a25fde225ab9d1f282
DESKTOP-PPPSA03\KAIFUJI <jikaifu.mel@gmail.com>
2018-07-18 21:33:26 +1000

    chapter 3

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,60 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 Page({
+  data:{
+    nowTemp: 'NoKnow',
+    nowWeather: 'NoKnow',
+    nowWeatherBG:'/images/sunny-bg.png'
+  },
+  GetWeatherNow(callback) {
+    console.log("Hello World");
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        this.setData({
+          nowTemp: temp,
+          nowWeather: weatherMap[weather],
+          nowWeatherBG: '/images/' + weather + '-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+
+      },
+      complete: ()=>{
+        callback && callback();
+      }
+    })
+  },
+  onLoad() {
+    this.GetWeatherNow();
+  },
+  onPullDownRefresh()
+  {
+    this.GetWeatherNow(wx.stopPullDownRefresh);
+  }
 })
